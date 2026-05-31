a84f16294a26401dece7ff049d3b324c4cb7c751
wmwm517 <apple051717@gmail.com>
2026-06-01 01:01:41 +0900

    fix: change sytle.css loading method

--- termProject/main.js
+++ termProject/main.js
@@ -1,5 +1,5 @@
 import App from './src/core/App.js';
-import './src/styles/style.css';
+// import './src/styles/style.css';
 
 window.addEventListener('DOMContentLoaded', () => {
     const app = new App();
