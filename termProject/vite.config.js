c145bfe4f3123ef2945da89a71e4949467f3dd3c
wmwm517 <apple051717@gmail.com>
2026-06-03 22:04:47 +0900

    fix: add GitHub Actions deploy workflow for ghpage

--- termProject/vite.config.js
+++ termProject/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/computerGraphics/termProject/',
+  base: '/computerGraphics/',
   publicDir: false,
   assetsInclude: ['**/*.obj'],
 })
